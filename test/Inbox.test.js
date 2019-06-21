d96b25466dd358824174599eba2a470afcb0453e
Jonathan Oliveira <jonathan@jonathanoliveira.org>
2019-06-20 22:17:30 -0400

    test the deployment of a contract into the ganache network

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,35 +3,46 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const fs = require('fs');
 const path = require('path');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider()
+const OPTIONS = {
+  defaultBlock: "latest",
+  transactionConfirmationBlocks: 1,
+  transactionBlockTimeout: 5
+};
+const web3 = new Web3(provider, null, OPTIONS);
+
+// reading JSON file contents
+let jsonOutputName = path.parse('Inbox').name + '.json';
+let jsonFile = './build/' + jsonOutputName;
+let contractJsonContent = fs.readFileSync(jsonFile, 'utf8');
+let jsonOutput = JSON.parse(contractJsonContent);
+
+// retrieving contract info
+let interface = jsonOutput['abi'];
+let bytecode = jsonOutput['evm']['bytecode']['object'];
 
 let accounts;
-let abi;
-let data;
+let inbox;
 
-beforeEach(async () => {
+beforeEach(async function () {
+  // getting a list of all accounts
+  accounts = await web3.eth.getAccounts();
 
-  // reading JSON file contents
-  let jsonOutputName = path.parse('Inbox').name + '.json';
-  let jsonFile = './build/' + jsonOutputName;
-  let contractJsonContent = fs.readFileSync(jsonFile, 'utf8');
-  let jsonOutput = JSON.parse(contractJsonContent);
-
-  // retrieving contract info
-  abi = jsonOutput['abi'];
-  data = jsonOutput['evm']['bytecode']['object'];
-
-  // get a list of all accounts
-  // accounts = await web3.eth.getAccounts();
-
-  // use one of those accounts to deploy the contract
+  // deploy contract into ganache network
+  inbox = await new web3.eth.Contract(interface).deploy({
+    data: bytecode,
+    arguments: ['My String']
+  }).send({
+    from: accounts[0],
+    gas: 1500000,
+    gasPrice: '30000000000000'
+  });
 });
 
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    // console.log(accounts);
-    // console.log(abi);
-    console.log(data);
+    console.log(inbox);
   });
 });
